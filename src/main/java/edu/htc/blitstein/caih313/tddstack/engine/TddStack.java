a10df7fe00ce44b825981acf6e575de0ad7c1f53
meahtc <55120690+meahtc@users.noreply.github.com>
2020-02-13 19:30:23 -0600

    Add a couple of methods to TddStack and test to TddStackTest as examples.

--- src/main/java/edu/htc/blitstein/caih313/tddstack/engine/TddStack.java
+++ src/main/java/edu/htc/blitstein/caih313/tddstack/engine/TddStack.java
@@ -1,4 +1,23 @@
 package edu.htc.blitstein.caih313.tddstack.engine;
 
+import edu.htc.blitstein.caih313.tddstack.IStackable;
+
 public class TddStack {
+
+    //to compile, this method must return something, but this
+    //code is just to make it compile, not a real implementation
+    boolean isEmpty(){
+        return false;
+    }
+
+    IStackable pop() {
+        //any object can be null; again, just to get it to compile
+        return null;
+    }
+
+    void push(IStackable iStackable) {
+        //doesn't need to return anything
+    }
+
+
 }
--- src/test/java/edu/htc/blitstein/caih313/tddstack/engine/TddStackTest.java
+++ src/test/java/edu/htc/blitstein/caih313/tddstack/engine/TddStackTest.java
@@ -1,7 +1,13 @@
 package edu.htc.blitstein.caih313.tddstack.engine;
 
+import edu.htc.blitstein.caih313.tddstack.IStackable;
+import edu.htc.blitstein.caih313.tddstack.resource.campus.Faculty;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class TddStackTest {
 
@@ -11,5 +17,21 @@ public class TddStackTest {
 
     @After
     public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testNewStackIsEmpty() {
+        TddStack tddStack = new TddStack();
+        assertTrue(tddStack.isEmpty());
+    }
+
+    @Test
+    public void testPopIsPush() {
+        TddStack tddStack = new TddStack();
+        //Faculty implements IStackable
+        IStackable iStackableOrig = new Faculty();
+        tddStack.push(iStackableOrig);
+        IStackable iStackablePopped = tddStack.pop();
+        assertEquals(iStackableOrig, iStackablePopped);
     }
 }
